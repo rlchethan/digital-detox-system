913f8f577ef0fa095fec16be11372adc07751587
Chethan R L <rlchethan0331@gmail.com>
2026-05-12 06:28:25 +0530

    Converted terminal project to Flask website

--- app.py
+++ app.py
@@ -1,41 +1,10 @@
-from flask import Flask, render_template, request
-import sqlite3
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/add', methods=['POST'])
-def add_usage():
-    app_name = request.form['app_name']
-    minutes = request.form['minutes']
-
-    conn = sqlite3.connect("detox.db")
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "INSERT INTO usage_logs(app_name, usage_minutes) VALUES (?, ?)",
-        (app_name, minutes)
-    )
-
-    conn.commit()
-    conn.close()
-
-    return "Usage Added Successfully"
-
-@app.route('/view')
-def view_usage():
-    conn = sqlite3.connect("detox.db")
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM usage_logs")
-    data = cursor.fetchall()
-
-    conn.close()
-
-    return render_template('usage.html', data=data)
+    return "Website Working!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
